8200065702654fc0020aac640bd3cf09da2e8543
jatin gyass <jatingyass9@gmail.com>
2025-04-07 22:51:14 +0530

    update db

--- config/db.js
+++ config/db.js
@@ -50,15 +50,41 @@
 
 
 
+
+// require('dotenv').config(); 
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('expenseZ', 'root', 'Jatin@123', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
+
+// sequelize.authenticate()
+//   .then(() => console.log('Database connected successfully!'))
+//   .catch(err => console.error('Database connection error:', err));
+
+// module.exports = sequelize;
+
+
+
+
+// database.js ya db.js (jo bhi tu use kar raha hai)
+require('dotenv').config(); // IMPORTANT
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('expenseZ', 'root', 'Jatin@123', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.RDS_ENDPOINT,
+    dialect: process.env.DIALECT
+  }
+);
 
 sequelize.authenticate()
-  .then(() => console.log('Database connected successfully!'))
-  .catch(err => console.error('Database connection error:', err));
+  .then(() => console.log('✅ Database connected successfully!'))
+  .catch(err => console.error('❌ Database connection error:', err));
 
 module.exports = sequelize;
